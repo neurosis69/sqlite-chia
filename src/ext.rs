c007cd70e1529f342c9dfd5323152d8ad323a370
Thomas Hein <thomashein@gmx.at>
2024-07-10 23:10:43 +0200

    pull from upstream, update dependencies and add chia_decompress_fullblock_json

--- src/ext.rs
+++ src/ext.rs
@@ -1,26 +1,28 @@
 // https://www.sqlite.org/loadext.html
 // https://github.com/jgallagher/rusqlite/issues/524#issuecomment-507787350
 
-use rusqlite::ffi;
+use std::os::raw::{c_char, c_int};
+use rusqlite::{ffi, Connection};
 
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
 #[no_mangle]
-pub extern "C" fn sqlite3_chia_init(
-    db_handle: *mut ffi::sqlite3,
-    _pz_err_msg: &mut &mut std::os::raw::c_char,
+pub unsafe extern "C" fn sqlite3_chia_init(
+    db: *mut ffi::sqlite3,
+    _pz_err_msg: *mut *mut c_char,
     p_api: *mut ffi::sqlite3_api_routines,
-) -> std::os::raw::c_int {
-    // SQLITE_EXTENSION_INIT2 equivalent
-    unsafe {
-        ffi::sqlite3_api = p_api;
+) -> c_int {
+    if p_api.is_null() {
+        return ffi::SQLITE_ERROR;
     }
-    match init(db_handle) {
-        Ok(()) => ffi::SQLITE_OK,
-        Err(_e) => ffi::SQLITE_ERROR,
+    if let Err(_err) = extension_init(db, p_api) {
+        return ffi::SQLITE_ERROR;
     }
+    ffi::SQLITE_OK
 }
 
-fn init(db_handle: *mut ffi::sqlite3) -> anyhow::Result<()> {
-    let db = unsafe { rusqlite::Connection::from_handle(db_handle)? };
+fn extension_init(db: *mut ffi::sqlite3, p_api: *mut ffi::sqlite3_api_routines) -> anyhow::Result<()> {
+    let db = unsafe { Connection::extension_init2(db, p_api)? };
     crate::setup(&db)?;
     Ok(())
 }
+
